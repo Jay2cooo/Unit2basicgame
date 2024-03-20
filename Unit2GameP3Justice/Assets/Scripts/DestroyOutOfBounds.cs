18f74c20108d84d6374d775124aa9c9e06020e9d
Jay2cooo <Onree.croffie@gmail.com>
2024-03-20 11:44:56 -0700

    About to be finished with project
    about to finish.

--- Unit2GameP3Justice/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2GameP3Justice/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
